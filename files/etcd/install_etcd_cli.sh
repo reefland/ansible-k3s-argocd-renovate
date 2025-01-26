a752c46617a6d6ba5df79961594ab3d868d470b7
Richard Durso <16230914+reefland@users.noreply.github.com>
2025-01-26 13:40:17 -0500

    FIX: Updated etcd port and wrapped variables in quotes per linter

--- files/etcd/install_etcd_cli.sh
+++ files/etcd/install_etcd_cli.sh
@@ -8,25 +8,29 @@
 #           ificates and install the CLI binary file.
 #          
 # AUTHOR     : Richard J. Durso
-# DATE       : 08/30/2022
-# VERSION    : 0.21
+# DATE       : 01/26/2025
+# VERSION    : 0.23
 #############################################################################
 
-if [ $(id -u) -ne 0 ]; then
+if [ "$(id -u)" -ne 0 ]; then
   echo
-  echo "* ERROR: ROOT privilage required to access ETCD certificates and install ETCD CLI."
+  echo "* ERROR: ROOT privilege required to access ETCD certificates and install ETCD CLI."
   echo
   exit 1
 fi
 
-ETCD_VER=v$(curl -L --cacert /var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert /var/lib/rancher/k3s/server/tls/etcd/server-client.crt --key /var/lib/rancher/k3s/server/tls/etcd/server-client.key -s https://127.0.0.1:2379/version | cut -d \" -f 4)
+#K3s older than ~v1.25 uses port 2379
+ETCD_PORT=2382
 
-echo Detected etcd server version: ${ETCD_VER}
+ETCD_VER="v$(curl -L --cacert /var/lib/rancher/k3s/server/tls/etcd/server-ca.crt --cert /var/lib/rancher/k3s/server/tls/etcd/server-client.crt --key /var/lib/rancher/k3s/server/tls/etcd/server-client.key -s https://127.0.0.1:${ETCD_PORT}/version | cut -d \" -f 4)"
 
-GOOGLE_URL=https://storage.googleapis.com/etcd
-GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+echo Detected etcd server version: "${ETCD_VER}"
+
 # Choose either URL
-DOWNLOAD_URL=${GOOGLE_URL}
+GOOGLE_URL=https://storage.googleapis.com/etcd
+# GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+
+DOWNLOAD_URL="${GOOGLE_URL}"
 
 # Determine if X86 or ARM architecture
 case "$(uname -m)" in
@@ -34,22 +38,22 @@ case "$(uname -m)" in
   x86_64) ETCD_ARCH="amd64" ;;
 esac;
 
-ETCD_NAME=etcd-${ETCD_VER}-linux-${ETCD_ARCH}
-ETCD_TAR=${ETCD_NAME}.tar.gz
+ETCD_NAME="etcd-${ETCD_VER}-linux-${ETCD_ARCH}"
+ETCD_TAR="${ETCD_NAME}.tar.gz"
 
-rm -f /tmp/${ETCD_TAR}
+rm -f "/tmp/${ETCD_TAR}"
 
-curl -L -s ${DOWNLOAD_URL}/${ETCD_VER}/${ETCD_TAR} -o /tmp/${ETCD_TAR}
+curl -L -s "${DOWNLOAD_URL}/${ETCD_VER}/${ETCD_TAR}" -o "/tmp/${ETCD_TAR}"
 
 # If tar fails, probably have an error message within the file, show it.
-if tar xzvf /tmp/${ETCD_TAR} -C /usr/local/bin --strip-components=1 ${ETCD_NAME}/etcdctl 2>/dev/null
+if tar xzvf "/tmp/${ETCD_TAR}" -C /usr/local/bin --strip-components=1 "${ETCD_NAME}/etcdctl" 2>/dev/null
 then
-  rm -f /tmp/${ETCD_TAR}
-  echo Installed: $(which etcdctl)
+  rm -f "/tmp/${ETCD_TAR}"
+  echo Installed: "$(which etcdctl)"
   etcdctl version
 else
   # Show error
-  cat /tmp/${ETCD_TAR}
+  cat "/tmp/${ETCD_TAR}"
   echo
-  rm -f /tmp/${ETCD_TAR}
+  rm -f "/tmp/${ETCD_TAR}"
 fi
